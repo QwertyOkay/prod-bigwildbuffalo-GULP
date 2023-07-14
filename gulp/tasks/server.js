8b37c316c610ba5cee6edb1e00fe69e3fefd62a3
Alexandr Kotenko <kpshmt@gmail.com>
2023-07-14 16:17:54 +0300

    update

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -7,31 +7,3 @@ export const server = (done) => {
     port: 3000,
   });
 };
-
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-
-// ...
-
-function server() {
-  browserSync.init({
-    server: {
-      baseDir: './dist', // Основная папка с компилированными файлами
-    },
-    routes: {
-      '/': './dist', // Маршрут для корневой страницы
-      '/about': './dist/about.html', // Маршрут для страницы about.html
-      '/contact': './dist/contact.html', // Маршрут для страницы contact.html
-      // Добавьте другие страницы и маршруты по необходимости
-    },
-  });
-
-  // ...
-}
-
-// ...
-
-exports.server = server;
-
-gulp.task('default', gulp.series('sass', 'js', 'html', 'server'));
-
